bbe3cb887e69686897485b1dd086db69026c491f
Harsh Goel <harshgoel2004@gmail.com>
2025-04-03 15:07:14 +0530

    sanity int-2

---
+++ .history/app/(frontend)/layout_20250403150708.tsx
@@ -0,0 +1,23 @@
+import Footer from '@/components/layout/footer'
+import Header from '@/components/layout/header'
+import WhatsAppWidget from '@/components/shared/whatsapp-widget'
+import React from 'react'
+
+const FrontendLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <>
+        <Header />
+          <main className="flex-grow">
+            {children}
+          </main>
+        <Footer />
+
+        <WhatsAppWidget 
+          phoneNumber='+917428487870'
+          position='left'
+        />
+    </>
+  )
+}
+
+export default FrontendLayout
